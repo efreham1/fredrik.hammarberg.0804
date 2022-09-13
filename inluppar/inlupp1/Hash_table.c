f1733ff66436ed5a8d02ecd9c6e1b7046d3d6d19
Fredrik Hammarberg <hammarberg83@gmail.com>
2022-09-13 16:57:33 +0200

    Co-authored-by: vfe01 <vfe01@users.noreply.github.com>

--- inluppar/inlupp1/Hash_table.c
+++ inluppar/inlupp1/Hash_table.c
@@ -15,11 +15,24 @@ struct entry
 
 struct hash_table
 {
-    entry_t *buckets[17];
+    entry_t buckets[17];
 };
 
+static entry_t *entry_create(int key, char *value, entry_t *next)
+{
+    entry_t *entry_p = calloc(1, sizeof(entry_t));
+    entry_t entry = {.key = key, .value = value, .next = next};
+    *entry_p = entry;
+    return entry_p;
+}
+
+static entry_t *find_previous_entry_for_key(entry_t *entry, int key)
+{
+    
+}
 //Create a new hash table
-ioopm_hash_table_t *ioopm_hash_table_create(void){
+ioopm_hash_table_t *ioopm_hash_table_create(void)
+{
     /// Allocate space for a ioopm_hash_table_t = 17 pointers to
     /// entry_t's, which will be set to NULL
     ioopm_hash_table_t *result = calloc(1, sizeof(ioopm_hash_table_t));
@@ -27,21 +40,39 @@ ioopm_hash_table_t *ioopm_hash_table_create(void){
 }
 
 //Delete a hash table and free its memory
-void ioopm_hash_table_destroy(ioopm_hash_table_t *ht){
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht)
+{
     free(ht);
 }
 
 //add key => value entry in hash table ht
-void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value){
-    //TODO funkar inte alls
-}
+void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value)
+{
+    /// Calculate the bucket for this entry
+    int bucket = key % 17;
+    /// Search for an existing entry for a key
+    entry_t *entry = find_previous_entry_for_key(&ht->buckets[bucket], key);
+    entry_t *next = entry->next;
+
+    /// Check if the next entry should be updated or not
+    if (next != NULL && next->key == key)
+    {
+        next->value = value;
+    }
+    else
+    {
+        entry->next = entry_create(key, value, next);
+    }
+    }
 
 //lookup value for key in hash table ht
-char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key){
+char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key)
+{
     return "Tjena"; //TODO funkar inte alls
 }
 
 //remove any mapping from key to a value
-char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key){
+char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key)
+{
     return "tjo bish"; //TODO funkar inte alls
 }
--- inluppar/inlupp1/Hash_table_test.c
+++ inluppar/inlupp1/Hash_table_test.c
@@ -1,5 +1,6 @@
 #include <CUnit/Basic.h>
 #include "Hash_table.h"
+#include <stdlib.h>
 
 int init_suite(void) {
   // Change this function if you want to do something *before* you
@@ -17,11 +18,72 @@ int clean_suite(void) {
 // functions of your own.
 void test_create_destroy()
 {
-   ioopm_hash_table_t *ht = ioopm_hash_table_create();
-   CU_ASSERT_PTR_NOT_NULL(ht);
-   ioopm_hash_table_destroy(ht);
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+  CU_ASSERT_PTR_NOT_NULL(ht);
+  ioopm_hash_table_destroy(ht);
 }
 
+void test_insert_once()
+{
+  int k = 5;
+  char *v = "BRUH";
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+  CU_ASSERT_PTR_NULL(ioopm_hash_table_lookup(ht, k));
+  ioopm_hash_table_insert(ht, k, v);
+  CU_ASSERT(ioopm_hash_table_lookup(ht, k)==v);
+  ioopm_hash_table_destroy(ht);
+}
+
+void test_insert_existing_key()
+{
+  int k = 5;
+  char *v = "BRUH";
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+  ioopm_hash_table_insert(ht, k, v);
+  char *v2 = "OOPS";
+  ioopm_hash_table_insert(ht, k, v2);
+  CU_ASSERT(ioopm_hash_table_lookup(ht, k)==v2);
+  ioopm_hash_table_destroy(ht);
+}
+
+void test_insert_changing_value()
+{
+  int k = 5;
+  char *v = "BRUH";
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+  ioopm_hash_table_insert(ht, k, v);
+  v = "Tjohejsan";
+  CU_ASSERT_STRING_EQUAL(ioopm_hash_table_lookup(ht, k), "BRUH");
+  ioopm_hash_table_destroy(ht);
+}
+
+void test_insert_existing_and_new_key()
+{
+  int k = 5;
+  char *v = "BRUH";
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+  ioopm_hash_table_insert(ht, k, v);
+  char *v2 = "OOPS";
+  ioopm_hash_table_insert(ht, k, v2);
+  CU_ASSERT(ioopm_hash_table_lookup(ht, k)==v2);
+  int k2 = 7;
+  char *v3 = "hallelujah";
+  ioopm_hash_table_insert(ht, k2, v3);
+  CU_ASSERT(ioopm_hash_table_lookup(ht, k2)==v3);
+  CU_ASSERT(ioopm_hash_table_lookup(ht, k)==v2);  
+  ioopm_hash_table_destroy(ht);
+}
+
+/*
+void test_insert_invalid_key()
+{
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();  
+  int k = 18;
+  char *v = "test";
+  ioopm_hash_table_insert(ht, k, v);
+  ioopm_hash_table_destroy(ht);
+}
+*/
 
 int main() {
   // First we try to set up CUnit, and exit if we fail
@@ -44,6 +106,10 @@ int main() {
   // copy a line below and change the information
   if (
     (CU_add_test(my_test_suite, "Test for create and destroy", test_create_destroy) == NULL) ||
+    (CU_add_test(my_test_suite, "Test for one insertion", test_insert_once) == NULL) ||
+    (CU_add_test(my_test_suite, "Test for insertion to an existing key", test_insert_existing_key) == NULL) ||
+    (CU_add_test(my_test_suite, "Test for changing the value outside of the hash table", test_insert_changing_value) == NULL) ||
+    (CU_add_test(my_test_suite, "Test for inserting a new key then an existing key and then a new key", test_insert_existing_and_new_key) == NULL) ||
     0
   )
     {
