2490562a299e5036bb8e5a1b66db48d376ea2c73
Fredrik Hammarberg <hammarberg83@gmail.com>
2022-09-29 18:04:53 +0200

    freq-count.c now works with the provided files

--- inluppar/inlupp1/freq-count.c
+++ inluppar/inlupp1/freq-count.c
@@ -21,12 +21,15 @@ void sort_keys(char *keys[], size_t no_keys)
 
 void process_word(char *word, ioopm_hash_table_t *ht)
 {
-  // FIXME: Rewrite to match your own interface, error-handling, etc.
-  int freq =
-    ioopm_hash_table_has_key(ht, (elem_t) {.ptr_v = word})?
-    (ioopm_hash_table_lookup(ht, (elem_t) {.ptr_v = word}))->int_v:
-    0;
-  ioopm_hash_table_insert(ht, (elem_t) {.ptr_v = strdup(word)}, (elem_t) {.int_v = freq + 1});
+  if (ioopm_hash_table_has_key(ht, (elem_t) {.ptr_v = word}))
+  {
+    int freq = ioopm_hash_table_lookup(ht, (elem_t) {.ptr_v = word})->int_v;
+    ioopm_hash_table_insert(ht, (elem_t) {.ptr_v = word}, (elem_t) {.int_v = freq + 1});
+  }
+  else
+  {
+    ioopm_hash_table_insert(ht, (elem_t) {.ptr_v = strdup(word)}, (elem_t) {.int_v = 1});
+  }
 }
 
 void process_file(char *filename, ioopm_hash_table_t *ht)
@@ -67,7 +70,7 @@ int string_sum_hash(elem_t e, int buckets)
       result += *str;
     }
   while (*++str != '\0');
-  return result%buckets; 
+  return abs(result%buckets); 
 }
 
 bool string_eq(elem_t e1, elem_t e2)
@@ -87,51 +90,50 @@ bool string_lt(elem_t e1, elem_t e2)
 
 int main(int argc, char *argv[])
 {
-  ioopm_hash_table_t *ht = ioopm_hash_table_create((ioopm_hash_function) string_sum_hash, string_eq, int_eq, string_lt);
-
   if (argc > 1)
   {
+    ioopm_hash_table_t *ht = ioopm_hash_table_create((ioopm_hash_function) string_sum_hash, string_eq, int_eq, string_lt);
     for (int i = 1; i < argc; ++i)
     {
       process_file(argv[i], ht);
     }
 
-    // FIXME: If the keys are returned as a list, transfer them into 
-    // an array to use `` (perhaps using an iterator?)
     ioopm_list_t *keys_list= ioopm_hash_table_keys(ht);
 
 
     int list_length = ioopm_linked_list_length(keys_list);
-    char *keys[list_length-1];
+    char *keys[list_length];
     ioopm_list_iterator_t *iter = ioopm_iterator_create(keys_list);
     int idx = 0;
-    while (ioopm_iterator_has_next(iter))
+    for(int i = 0; i < list_length; i++)
     {
       keys[idx] = ioopm_iterator_current(iter).ptr_v;
       idx++;
       ioopm_iterator_next(iter);
     }
-    ioopm_iterator_destroy(iter);
-    ioopm_linked_list_destroy(keys_list);
 
-    sort_keys(keys, list_length-1);
+    sort_keys(keys, list_length);
 
     FILE *file = fopen("output.txt", "w");
-    for (int i = 0; i < list_length-1; ++i)
+    for (int i = 0; i < list_length; ++i)
     {
       // FIXME: Update to match your own interface, error handling, etc.
       int freq = (ioopm_hash_table_lookup(ht, (elem_t) {.ptr_v = keys[i]}))->int_v;
       fprintf(file, "%s: %d\n", keys[i], freq);
     }
     fclose(file);
+
+    for (int i = 0; i < list_length; i++)
+  {
+    free(keys[i]);
+  }
+  ioopm_iterator_destroy(iter);
+  ioopm_linked_list_destroy(keys_list);
+  ioopm_hash_table_destroy(ht);
   }
   else
   {
     puts("Usage: freq-count file1 ... filen");
   }
-
-  // FIXME: Leaks memory! Use valgrind to find out where that memory is 
-  // being allocated, and then insert code here to free it.
-  ioopm_hash_table_destroy(ht);
 }
 
